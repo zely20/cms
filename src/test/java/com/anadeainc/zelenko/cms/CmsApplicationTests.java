43b1316da4fc4eea675f50e73bc325d98e8e4097
Aliaksandr Zelianko <zely20@mail.ru>
2021-11-22 14:36:37 +0300

    Initial commit

---
+++ src/test/java/com/anadeainc/zelenko/cms/CmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anadeainc.zelenko.cms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
